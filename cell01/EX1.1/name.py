c3395436dc142c07ccec6c6666f771165a4a0753
guscokeza <rxdchyantphachra@gmail.com>
2025-03-10 13:43:21 +0700

    Add files via upload

---
+++ cell01/EX1.1/name.py
@@ -0,0 +1,9 @@
+# Concatenate last name and first name
+last_name = "Suwannahong"  # Replace with your last name
+first_name = "Yadnamtip"  # Replace with your first name
+
+# Concatenate to form the whole name
+whole_name = last_name + " " + first_name
+
+# Display the whole name followed by a new line
+print(whole_name)
