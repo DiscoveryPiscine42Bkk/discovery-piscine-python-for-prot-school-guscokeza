0d9587acc3ef5938c037c56ed029ee3120042a70
guscokeza <rxdchyantphachra@gmail.com>
2025-03-10 12:10:49 +0700

    Add files via upload

---
+++ cell01/EX1.0/name.py
@@ -0,0 +1,9 @@
+# name.py
+
+# กำหนดตัวแปร first_name และ last_name
+first_name = "Yadnamtip"  # แทนที่ "YourFirstName" ด้วยชื่อจริงของคุณ
+last_name = "Suwannahong"    # แทนที่ "YourLastName" ด้วยนามสกุลจริงของคุณ
+
+# แสดงผลลัพธ์
+print(first_name)
+print(last_name)
