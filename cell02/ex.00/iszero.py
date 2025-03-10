70ea031ee5cec18d946b4cd583a7638a8bc7371e
guscokeza <rxdchyantphachra@gmail.com>
2025-03-10 14:31:23 +0700

    Add files via upload

---
+++ cell02/ex.00/iszero.py
@@ -0,0 +1,8 @@
+# Ask the user to input a number
+number = float(input("Please enter a number: "))
+
+# Check if the number is equal to zero or not
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
