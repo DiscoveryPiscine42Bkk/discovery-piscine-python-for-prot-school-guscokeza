36ad59107991f371c81f375b16b7cfe3da11f68b
guscokeza <rxdchyantphachra@gmail.com>
2025-03-10 13:52:32 +0700

    Add files via upload

---
+++ cell01/EX1.2/age.py
@@ -0,0 +1,8 @@
+# Assign your age to the variable
+my_age = 15  # Replace with your actual age
+
+# Add 42 to your age and assign the result to my_age
+my_age += 42
+
+# Display the result of my_age followed by a new line
+print(my_age)
