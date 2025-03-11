b8d68f43e3c8956ef6f940631c858c79c1ecb2f1
guscokeza <rxdchyantphachra@gmail.com>
2025-03-11 09:18:50 +0700

    Add files via upload

---
+++ cell02/EX.02/password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    # Define the password variable
+    password = "1234"
+
+    # Optionally, you can print or use the password variable
+    print(f"The stored password is: {password}")
+
+if __name__ == "__main__":
+    main()
